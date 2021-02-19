a66aee52cce487fcf0459d8173af11f2ccb5814c
Ediberto Oliveira <edibertooliveira@aol.com>
2021-02-19 10:48:11 -0300

    feat:  initialaze project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style/main.css';
+import App from '../src/screen/TicTacToe';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
