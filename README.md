53439b0a11ab61b3a6d652744f63f82c95b2184a
Ediberto Oliveira <edibertooliveira@aol.com>
2021-03-13 09:47:46 -0300

    feat: added btn reset game

---
+++ README.md
@@ -0,0 +1,2 @@
+_React Project applying the knowledge studied in class to make the tic-tac-toe game work_
+Link: tic-tac-toe-git-master.edibertooliveira.vercel.app
--- src/screen/TicTacToe/index.jsx
+++ src/screen/TicTacToe/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import GameBoard from '../../components/GameBoard';
+import Confetti from '../../components/Confetti';
 import './style.css';
 
 const returnWin = [
@@ -19,74 +20,107 @@ const returnWin = [
   ],
 ];
 
+const INITIAL_STATE = {
+  activePlayer: 1,
+  playerX: [],
+  playerO: [],
+  gameBoard: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  endGame: false,
+};
+
+const messageReset = () => {
+  const messages = [
+    'Vamos outra! Reinicie o jogo',
+    'Desafia ir outra. Reinicie o jogo',
+    'Vai outra vez! Reinicie o jogo',
+    'Que tal mais uma? Reinicie o jogo',
+    'Dá tempo pra outra? Reinicie o jogo',
+  ];
+  const random = Math.round(Math.random() * messages.length - 1);
+  return messages[random];
+};
+
 class TicTacToe extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      activePlayer: 1,
-      playerX: [],
-      playerO: [],
-      gameBoard: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-      endGame: false,
-    };
+    this.state = INITIAL_STATE;
 
     this.handlePlayersMove = this.handlePlayersMove.bind(this);
     this.winningConditions = this.winningConditions.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   winningConditions(playerCurrent, activePlayer) {
     const [diagonal, vestical, horizontal] = returnWin;
     const checked =
-      diagonal.find(value1 => value1[1].every(value2 => playerCurrent.includes(value2))) ||
-      vestical.find(value1 => value1[1].every(value2 => playerCurrent.includes(value2))) ||
-      horizontal.find(value1 => value1[1].every(value2 => playerCurrent.includes(value2)));
-      if(checked) {
-        activePlayer = activePlayer === 1 ? 2 : 1
-        this.setState(state => ({ ...state, activePlayer }));    
-      }
-    this.setState(state => ({ ...state, endGame: checked }));
+      diagonal.find((value1) =>
+        value1[1].every((value2) => playerCurrent.includes(value2))
+      ) ||
+      vestical.find((value1) =>
+        value1[1].every((value2) => playerCurrent.includes(value2))
+      ) ||
+      horizontal.find((value1) =>
+        value1[1].every((value2) => playerCurrent.includes(value2))
+      );
+    if (checked) {
+      activePlayer = activePlayer === 1 ? 2 : 1;
+      this.setState((state) => ({ ...state, activePlayer }));
+    }
+    this.setState((state) => ({ ...state, endGame: checked }));
   }
 
   handlePlayersMove(position) {
-    let { activePlayer, gameBoard, endGame} = this.state;
+    let { activePlayer, gameBoard, endGame } = this.state;
     if (!endGame) {
-      
       const player = activePlayer === 1 ? 'X' : 'O';
       const newGameBoard = [];
-      const activeRound =  player === 'X' ? (activePlayer = 2) : (activePlayer = 1);
-      
+      const activeRound =
+        player === 'X' ? (activePlayer = 2) : (activePlayer = 1);
+
       gameBoard.forEach((field, index) => {
         if (index === position) field = player;
         newGameBoard.push(field);
       });
-      
-      this.setState(state => ({
+
+      this.setState((state) => ({
         [`player${player}`]: [...state[`player${player}`], position],
         activePlayer: activeRound,
         gameBoard: newGameBoard,
       }));
-      this.winningConditions([...this.state[`player${player}`], position], activeRound);
+      this.winningConditions(
+        [...this.state[`player${player}`], position],
+        activeRound
+      );
     }
   }
-    
+
+  handleClick() {
+    this.setState({ ...INITIAL_STATE })
+  }
+
   render() {
     const { gameBoard, endGame, activePlayer } = this.state;
 
     if (endGame) {
       return (
         <section className="ticTacToe">
-          <h1>{`Fim de Jogo`}</h1>
-          <p>{` Vitoria ${endGame[0]} do Player ${activePlayer}`}</p>
+          <Confetti />
+          <h2>{`Fim de Jogo`}</h2>
+          <h1>{` Vitoria ${endGame[0]} do Player ${activePlayer}`}</h1>
+          <button
+          style= {{ zIndex: 10}}
+            type="button"
+            onClick={ this.handleClick }
+          >
+            {messageReset()}
+          </button>
         </section>
       );
     }
     return (
       <section className="ticTacToe">
         <h1>Player {activePlayer}</h1>
-        <GameBoard
-          gameState={gameBoard}
-          onClick={this.handlePlayersMove}
-        />
+        <GameBoard gameState={gameBoard} onClick={this.handlePlayersMove} />
       </section>
     );
   }
